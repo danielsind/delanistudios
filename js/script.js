8ee1d5e343adb03cc4f21c91752f5a40680b7c4e
danielsind <danielsindet@gmail.com>
2019-12-08 21:12:39 +0300

    add hover on portfolio

--- js/script.js
+++ js/script.js
@@ -8,4 +8,28 @@ $("#developmentimg").click(function(){
 $("#productimg").click(function(){
     $(".product").toggle()
 })
+$(".wk1").hover(function(){
+    $(".appear1").toggle()
+})
+$(".wk2").hover(function(){
+    $(".appear2").toggle()
+})
+$(".wk3").hover(function(){
+    $(".appear3").toggle()
+})
+$(".wk4").hover(function(){
+    $(".appear4").toggle()
+})
+$(".wk5").hover(function(){
+    $(".appear5").toggle()
+})
+$(".wk6").hover(function(){
+    $(".appear6").toggle()
+})
+$(".wk7").hover(function(){
+    $(".appear7").toggle()
+})
+$(".wk8").hover(function(){
+    $(".appear8").toggle()
+})
 });
